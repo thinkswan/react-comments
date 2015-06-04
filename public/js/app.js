779efe4ae9a6af8bf0ef8280e2e3968ed18f51f3
Graham Swan <thinkswan@gmail.com>
2015-06-04 12:21:28 +0700

    Build CommentList and CommentForm components

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,50 @@
+/*
+  CommentBox
+ */
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
 });
+
+/*
+  CommentList
+ */
+
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+/*
+  CommentForm
+ */
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+/*
+  Initialize the app
+ */
 
 React.render(
   <CommentBox />,

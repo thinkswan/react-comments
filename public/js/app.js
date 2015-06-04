3f7fbad642da740ee4231ba4b9ebe038f81198e4
Graham Swan <thinkswan@gmail.com>
2015-06-04 12:25:55 +0700

    Build Comment component

--- public/js/app.js
+++ public/js/app.js
@@ -22,7 +22,27 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
+      </div>
+    );
+  }
+});
+
+/*
+  Comment
+ */
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+
+        {marked(this.props.children.toString())}
+
       </div>
     );
   }

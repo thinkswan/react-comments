d8aeea70cd806fc1a78437bec67999821d10f20b
Graham Swan <thinkswan@gmail.com>
2015-06-04 12:28:42 +0700

    Use data map to handle comments

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -20,10 +20,17 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.author}>
+          {comment.text}
+        </Comment>
+      );
+    });
+
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -68,7 +75,12 @@ var CommentForm = React.createClass({
   Initialize the app
  */
 
+var data = [
+  { author: "Pete Hunt", text: "This is one comment" },
+  { author: "Jordan Walke", text: "This is *another* comment" }
+];
+
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
